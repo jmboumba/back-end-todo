f053c960a9d546c570700ab80ecb29983e07b1c6
MBOUMBA MOUSSAVOU Junior Melcki <juniormboumba98@gmail.com>
2024-11-09 23:58:22 +0100

    Modification des données de DB

--- db_connect.js
+++ db_connect.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-
-const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
+const db = mysql.createConnection({
+    host: process.env.DB_HOST, // or your database host
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+  });
 
 db.connect((err) => {
     if (err) {

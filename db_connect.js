a3b8493c6b94d12050b724d3661720d57e0b3af7
MBOUMBA MOUSSAVOU Junior Melcki <juniormboumba98@gmail.com>
2024-11-10 00:01:02 +0100

    Modification des données de DB 2

--- db_connect.js
+++ db_connect.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST, // or your database host
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   });
 
 db.connect((err) => {

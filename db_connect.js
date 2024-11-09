b9d7ef4fb7e0099e8fe1b3705009e44482bd3cb8
MBOUMBA MOUSSAVOU Junior Melcki <juniormboumba98@gmail.com>
2024-11-10 00:52:20 +0100

    Modification des données de DB 4

--- db_connect.js
+++ db_connect.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
 const db = mysql.createConnection(urlDB);
 
 db.connect((err) => {
